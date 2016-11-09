75790637fd4ac9897fe37aad97e99c794eeb976e
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-09 13:38:00 +0100

    Update# 73

--- src/Contents/Application/app/view/VSaisie.js
+++ src/Contents/Application/app/view/VSaisie.js
@@ -28,77 +28,95 @@ initComponent: function()
 			layout: "hbox",
 			width: "100%",
 			padding: 10,
-			border: false,
+			border: false,			
 			items: [
 			{
-				xtype: "combo",
-				fieldLabel: "Famille",
-				labelAlign: "top"
+				width: 250	
 			},
 			{
-				xtype: "combo",
-				fieldLabel: "Agent",
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "combo",
-				fieldLabel: "Végétation",
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "datefield",
-				fieldLabel: "Date visite",
-				labelAlign: "top",
-				margin:{left: 5},
-				flex: 1
-			}
-			]
-		},
-		{
-			layout: "hbox",
-			width: "100%",
-			padding: 10,
-			border: false,
-			items: [
-			{
-				xtype: "combo",
-				fieldLabel: "Type",
-				labelAlign: "top"
-			},
-			{
-				xtype: "combo",
-				fieldLabel: "Agent",
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "combo",
-				fieldLabel: "Météo",
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "textfield",
-				fieldLabel: "Hauteur",
-				width: 50,
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "textfield",
-				fieldLabel: "Longueur",
-				width: 50,
-				labelAlign: "top",
-				margin:{left: 5}
-			},
-			{
-				xtype: "textfield",
-				fieldLabel: "Surface",
-				labelAlign: "top",
 				flex: 1,
-				margin:{left: 5}
+				layout: "vbox",
+				width: "100%",
+				items: [
+				{
+					layout: "hbox",
+					width: "100%",
+					padding: 10,
+					border: false,
+					items: [
+					{
+						xtype: "combo",
+						fieldLabel: "Famille",
+						labelAlign: "top"
+					},
+					{
+						xtype: "combo",
+						fieldLabel: "Agent",
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "combo",
+						fieldLabel: "Végétation",
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "datefield",
+						fieldLabel: "Date visite",
+						labelAlign: "top",
+						margin:{left: 5},
+						flex: 1
+					}
+					]
+				},
+				{
+					layout: "hbox",
+					width: "100%",
+					padding: 10,
+					border: false,
+					items: [
+					{
+						xtype: "combo",
+						fieldLabel: "Type",
+						labelAlign: "top"
+					},
+					{
+						xtype: "combo",
+						fieldLabel: "Agent",
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "combo",
+						fieldLabel: "Météo",
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "textfield",
+						fieldLabel: "Hauteur",
+						width: 50,
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "textfield",
+						fieldLabel: "Longueur",
+						width: 50,
+						labelAlign: "top",
+						margin:{left: 5}
+					},
+					{
+						xtype: "textfield",
+						fieldLabel: "Surface",
+						labelAlign: "top",
+						flex: 1,
+						margin:{left: 5}
+					}
+					]
+				}					
+				]
 			}
 			]
 		},
