be25b85dc8804843cd1925ccec09c17b20f02032
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 20:04:04 +0100

    Update# 328

--- src/System/app.js
+++ src/System/app.js
@@ -3,20 +3,22 @@ App = {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));	
 		app.post('/evt',function(req,res){
 			res.header("Content-Type", "application/json; charset=utf-8");
-			var ff=[];			
-			var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType where idParent=1";
-			var db=App.using('db');
-			db.query("gopro",sql,function(e,o){
-				for (var i=0;i<o.length;i++)
-				{
-					ff.push({
-						text: o[i].nomElement,
-						id: 'K'+i,
-						leaf: false
-					});
-				};
-				res.end(JSON.stringify(ff,null,4));	
-			});
+			var ff=[];	
+			if (req.body.node=="root") {
+				var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType where idParent=1";
+				var db=App.using('db');
+				db.query("gopro",sql,function(e,o){
+					for (var i=0;i<o.length;i++)
+					{
+						ff.push({
+							text: o[i].nomElement,
+							id: 'K'+i,
+							leaf: false
+						});
+					};
+					res.end(JSON.stringify(ff,null,4));	
+				});
+			}
 		});
 	}
 };
