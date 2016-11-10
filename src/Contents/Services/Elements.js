b01c4ec8661b2a77a451b11ced99225026ae2b99
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 15:06:50 +0100

    Update# 215

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -2,7 +2,7 @@ Elements={
 	getTree: function(o,cb) {
 		var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType order by idParent";
 		var db=Elements.using('db');
-		db.query(sql,function(e,r){
+		db.query("gopro",sql,function(e,r){
 			var root=[];
 			var obj={};
 			for (var i=0;i<r.length;i++) {
