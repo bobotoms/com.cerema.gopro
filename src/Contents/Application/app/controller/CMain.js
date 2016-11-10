7c47c6726046940f97bb4e08dd7856102c4d66a6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 16:36:59 +0100

    Update# 284

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -67,18 +67,29 @@ App.controller.define('CMain', {
 		App.Elements.getTree({},function(response){
 			console.log(response);
 
-			response.isRoot=false;
-			var json = response;
+			response.isRoot=true;
+			var json = [
+			{
+				success:true,
+				isRoot:true, 
+				name: "root",
+				text: "Eléments",
+				children: response
+			}
+			];
 
             //var jsonstr_decoded = Ext.JSON.decode(jsonstr);
             var reader = App.get(me,'treepanel').getStore().getProxy().getReader();
           	var reader_data = reader.read(json);
 
             // First read, then load it here.
-			App.get(me,'treepanel').getStore().root.removeAll();
+			//App.get(me,'treepanel').getStore().root.removeAll();
             App.get(me,'treepanel').getStore().loadData(reader_data.records,true);			
 			
-
+			//App.get(me,'treepanel').bindStore(store);
+			
+			//console.log(store);
+			//App.get(me,'treepanel').getStore().loadData(root);
 		})
 	},
 	add_item_click: function() {
