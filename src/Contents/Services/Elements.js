b197d0866aea885f41d95502aa274ac14bb8f958
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 14:57:08 +0100

    Update# 213

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -1,5 +1,14 @@
 Elements={
-	
+	getTree: function(o,cb) {
+		var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType order by idParent";
+		var db=Elements.using('db');
+		db.query(sql,function(e,r){
+			var root=[];
+			for (var i=0;i<r.length;i++) {
+				
+			}
+		});
+	}
 };
 
 module.exports=Elements;
