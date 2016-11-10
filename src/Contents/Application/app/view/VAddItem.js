e354a411712b6e3dd595da0da938e17bd7f7abf3
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 19:51:35 +0100

    Update# 319

--- src/Contents/Application/app/view/VAddItem.js
+++ src/Contents/Application/app/view/VAddItem.js
@@ -19,7 +19,7 @@ App.view.define('VAddItem',{
 			height: "100%",
 			border: false,
 			flex: 1,
-			rootVisible: true,
+			rootVisible: false,
 			useArrows: true,
 			expanded: true,
 			columns: [{
