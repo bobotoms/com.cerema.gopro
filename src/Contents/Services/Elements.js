b6a43afb6bdc045218311313dc78bd49af96c707
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 15:06:32 +0100

    Update# 214

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -4,9 +4,17 @@ Elements={
 		var db=Elements.using('db');
 		db.query(sql,function(e,r){
 			var root=[];
+			var obj={};
 			for (var i=0;i<r.length;i++) {
-				
+				var id=r[i].idElement;
+				var parent=r[i].idElement;
+				if (!obj[id]) obj[id]={
+					text: r[i].nomElement,
+					children:[]
+				};
+				if (parent==1) root.push(obj[id]); else obj[parent].children.push(obj[id]);
 			}
+			cb(root);
 		});
 	}
 };
