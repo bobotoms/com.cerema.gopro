9ebdb53dcbb036dd1b92180ac14e74bf993f3072
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 14:05:43 +0100

    Update# 192

--- src/Contents/Application/app/view/VAddItem.js
+++ src/Contents/Application/app/view/VAddItem.js
@@ -1,3 +1,21 @@
 App.view.define('VAddItem',{
-	
+	extend: 'Ext.window.Window',
+	alias: "widget.VAddItem",
+
+	initComponent: function()
+	{
+		this.title="Catalogue";
+		this.layout="vbox";
+		this.border=false;
+		this.width = 700;
+        this.height = 430;
+		this.bodyStyle="background-color: white";
+        this.bbar = [
+            
+        ];	
+		this.items = [
+		
+		];
+		this.callParent();
+	}
 });
