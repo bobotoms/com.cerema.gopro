9fe0aa7e23b69804a37d2b5f175d700ed5c45821
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 20:53:14 +0100

    Update# 346

--- src/System/app.js
+++ src/System/app.js
@@ -6,31 +6,33 @@ App = {
 			var db=App.using('db');
 			var ff=[];	
 			if (req.body.node=="root") {
-				var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType where idParent=1";
-				db.query("gopro",sql,function(e,o){
-					for (var i=0;i<o.length;i++)
-					{
-						ff.push({
-							text: o[i].nomElement,
-							id: 'K_'+i,
-							leaf: false
-						});
-					};
-					res.end(JSON.stringify(ff));	
-				});
+				
+				var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType order by idParent";
+				var db=Elements.using('db');
+				db.query("gopro",sql,function(e,r){
+					var root=[];
+					var obj={};
+					for (var i=0;i<r.length;i++) {
+						var id=r[i].idElement;
+						var parent=r[i].idParent;
+						if (!obj[id]) obj[id]={
+							id: i,
+							name: "c"+i,
+							text: r[i].nomElement,
+							leaf: true
+						};
+						if (parent==1) root.push(obj[id]); else {
+							if (!obj[parent].children) {
+								obj[parent].children=[];
+								obj[parent].leaf=false;
+							};
+							obj[parent].children.push(obj[id]);
+						}
+					}
+					res.end(JSON.stringify(root));
+				});				
 			} else {
-				var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType where idParent="+req.body.node.split('_')[1];
-				db.query("gopro",sql,function(e,o){
-					for (var i=0;i<o.length;i++)
-					{
-						ff.push({
-							text: o[i].nomElement,
-							id: 'L'+i,
-							leaf: false
-						});
-					};
-					res.end(JSON.stringify(ff));	
-				});
+				
 			}
 		});
 	}
