9227a6be2482fd8e01b31eb235a7e4d66fbb7538
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 09:47:19 +0100

    Update# 107

--- src/Contents/Application/app/view/VSaisie.js
+++ src/Contents/Application/app/view/VSaisie.js
@@ -27,176 +27,31 @@ initComponent: function()
 		{
 			layout: "hbox",
 			width: "100%",
-			padding: 10,
-			border: false,			
+			height: 150,
 			items: [
 			{
-				width: 200,
-				height: "100%"
+				border: true,
+				width: 200
 			},
 			{
-				flex: 1,
+				padding: 10,
 				layout: "vbox",
-				width: "100%",
-				border: false,
+				flex: 1,
+				height: "100%",
 				items: [
 				{
 					layout: "hbox",
-					width: "100%",
-					padding: 10,
-					border: false,
 					items: [
 					{
 						xtype: "combo",
 						fieldLabel: "Famille",
 						labelAlign: "top"
-					},
-					{
-						xtype: "combo",
-						fieldLabel: "Agent",
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "combo",
-						fieldLabel: "Végétation",
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "datefield",
-						fieldLabel: "Date visite",
-						labelAlign: "top",
-						margin:{left: 5},
-						flex: 1
-					}
-					]
-				},
-				{
-					layout: "hbox",
-					width: "100%",
-					padding: 10,
-					border: false,
-					items: [
-					{
-						xtype: "combo",
-						fieldLabel: "Type",
-						labelAlign: "top"
-					},
-					{
-						xtype: "combo",
-						fieldLabel: "Agent",
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "combo",
-						fieldLabel: "Météo",
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "textfield",
-						fieldLabel: "Hauteur",
-						width: 50,
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "textfield",
-						fieldLabel: "Longueur",
-						width: 50,
-						labelAlign: "top",
-						margin:{left: 5}
-					},
-					{
-						xtype: "textfield",
-						fieldLabel: "Surface",
-						labelAlign: "top",
-						flex: 1,
-						margin:{left: 5}
-					}
-					]
-				},
-				{
-					layout: "hbox",
-					width: "100%",
-					padding: 10,
-					border: false,
-					items: [
-					{
-						html: "Géolocalisation",
-						border: false
-					},
-					{
-						xtype: "textfield",
-						width: 100
-					},
-					{
-						xtype: "textfield",
-						width: 100
 					}
 					]
 				}
 				]
 			}
 			]
-		},
-		{
-			layout: "hbox",
-			flex: 1,
-			padding: 10,
-			width: "100%",
-			border: false,
-			items: [
-			{
-				xtype: "grid",
-				columns: [{
-					header: "",
-					width: 100
-				},
-				{
-					header: "",
-					width: 10
-				},
-				{
-					header: "",
-					width: 10
-				},
-				{
-					header: "",
-					width: 10
-				},
-				{
-					header: "",
-					width: 10
-				},
-				{
-					header: "",
-					width: 10
-				},
-				{
-					header: "",
-					width: 100
-				}],
-				store: App.store.create({fields:["","","","","","",""],data:[]}),
-				height: "100%",
-				flex: 2
-			},
-			{
-				html: "",
-				flex: 1,
-				height: "100%"
-			}
-			]
-		},
-		{
-			xtype: "htmleditor",
-			width: "100%",
-			fieldLabel: "Synthèse de la visite",
-			labelAlign: "top",
-			padding: 10,
-			height: 200
 		}
 		];
 		this.callParent();
