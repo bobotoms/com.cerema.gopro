2154e5771c8a1f60191d3d54b6f1f1d6962f271d
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 16:39:38 +0100

    Update# 288

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -65,7 +65,7 @@ App.controller.define('CMain', {
 	},
 	VAddItem_onShow: function(me) {
 		App.Elements.getTree({},function(response){
-			console.log(response);
+			//console.log(response);
 
 			response.isRoot=true;
 			var json = [
