d64476cb8eb3f4a78d6ec668f6456af4c6e61a07
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-10 20:00:17 +0100

    Update# 325

--- src/System/app.js
+++ src/System/app.js
@@ -2,7 +2,21 @@ App = {
 	init: function(app,server) {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));	
 		app.post('/evt',function(req,res){
-			
+			res.header("Content-Type", "application/json; charset=utf-8");
+			var ff=[];			
+			var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType where idParent=1";
+			var db=App.using('db');
+			db.query("gopro",sql,function(e,r){
+				for (var i=0;i<o.length;i++)
+				{
+					ff.push({
+						text: o[i].nomElement,
+						id: 'Kets'+i,
+						leaf: false
+					});
+				};
+				res.end(JSON.stringify(ff,null,4));	
+			});
 		});
 	}
 };
