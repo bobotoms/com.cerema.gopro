c6f7008916603fbf2fc78439f89f4233450693a4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-14 12:57:55 +0100

    Update# 383

--- src/System/app.js
+++ src/System/app.js
@@ -6,7 +6,7 @@ App = {
 			var db=App.using('db');
 			var ff=[];	
 			if (req.body.node=="root") {
-				var sql="SELECT idElement,parent,new_elements.idType_element,new_elements.nomElement,modif FROM new_elements join types_elements on new_elements.idType_element=types_elements.idType where parent=0 and new_elements.idType=1";
+				var sql="SELECT idElement,parent,new_elements.idType_element,new_elements.nomElement,modif FROM new_elements join types_elements on new_elements.idType_element=types_elements.idType where parent=0 and new_elements.idType=1 order by NomElement";
 				var db=Elements.using('db');
 				db.query("gopro",sql,function(e,r){
 					var root=[];
