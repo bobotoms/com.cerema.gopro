4ba6d54d56f262f26b8207021695b0029fb805c2
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-14 10:54:25 +0100

    Update# 364

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -2,6 +2,7 @@ Elements={
 	getTree: function(o,cb) {
 		var sql="SELECT idElement,idParent,elements.idType,elements.nomElement,modif FROM elements join types_elements on elements.idType=types_elements.idType order by idParent";
 		var db=Elements.using('db');
+		if (!o.q) {
 		db.query("gopro",sql,function(e,r){
 			var root=[];
 			var obj={};
@@ -24,6 +25,31 @@ Elements={
 			}
 			cb(root);
 		});
+		} else {
+			db.query("gopro",sql,function(e,r){
+				var root=[];
+				var droot=[];
+				var obj={};
+				for (var i=0;i<r.length;i++) {
+					var id=r[i].idElement;
+					var parent=r[i].idParent;
+					if (!obj[id]) obj[id]={
+						id: i,
+						name: "c"+i,
+						text: r[i].nomElement,
+						leaf: true
+					};
+					if (parent==1) root.push(obj[id]); else {
+						if (!obj[parent].children) {
+							obj[parent].children=[];
+							obj[parent].leaf=false;
+						};
+						obj[parent].children.push(obj[id]);
+					}
+				}
+				cb(root);
+			});			
+		}
 	}
 };
 
