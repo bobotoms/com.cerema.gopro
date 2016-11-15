1630114bbe9f9c6919c51a5cf748d4942e292912
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-15 16:39:31 +0100

    Update# 560

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -34,7 +34,7 @@ Elements={
 			};
 			var i=1;
 			var objs=[];
-			if (obj[itemId].leaf) obj[itemId].text="<b>"+obj[itemId].text+"</b>";
+			if (!obj[itemId].leaf) obj[itemId].text="<b>"+obj[itemId].text+"</b>";
 			console.log(obj[itemId]);
 			while (itemId!=0) {
 				objs.push(O[itemId]);
