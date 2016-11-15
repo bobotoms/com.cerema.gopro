6723141a9f225f1a152fa0bab24ee6899630d94b
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-15 14:35:46 +0100

    Update# 507

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -74,23 +74,23 @@ App.controller.define('CMain', {
         var selModel = tree.getSelectionModel();
         var node = selModel.getLastSelected();
 		console.log(node);
-		console.log(App.get(me.up('window'),"treepanel#T1").getRootNode());
-		App.get(me.up('window'),"treepanel#T1").getRootNode().appendChild(node.parentNode);
-		/*App.Elements.getSelect(node.data.id,me.up('window').type_item,function(r){
+		/*console.log(App.get(me.up('window'),"treepanel#T1").getRootNode());
+		App.get(me.up('window'),"treepanel#T1").getRootNode().appendChild(node.parentNode);*/
+		App.Elements.getSelect(node.data.id,me.up('window').type_item,function(r){
 			console.log(r);
-			/*for (var i=0;i<r.length;i++) {
+			for (var i=0;i<r.length;i++) {
 				var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(r[i].id);
 				if (!xnode) App.get(me.up('window'),"treepanel#T1").getRootNode().appendChild(r[i]);
 			}
-			//var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(pp.data.id);
+			/*var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(pp.data.id);
 			//console.log(xnode);
 			App.get(me.up('window'),"treepanel#T1").getStore().loadData(r);
 			App.get(me.up('window'),"treepanel#T1").getStore().on('load',function(){
 			alert(App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(r[0].id));	
-			});
+			});*/
 			
 			
-		});*/
+		});
 		App.get(me.up('window'),"treepanel#T1").expandAll();
 		
 	},
