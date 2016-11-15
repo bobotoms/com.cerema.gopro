4fc5061630510e485c2163fb22f29bf3b3050ccc
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-15 12:45:21 +0100

    Update# 495

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -76,8 +76,8 @@ App.controller.define('CMain', {
 		App.Elements.getSelect(node.data.id,me.up('window').type_item,function(r){
 			console.log(r);
 			for (var i=0;i<r.length;i++) {
-				var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(r[0].id);
-				if (!xnode) App.get(me.up('window'),"treepanel#T1").getRootNode().appendChild(r[0]);
+				var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(r[i].id);
+				if (!xnode) App.get(me.up('window'),"treepanel#T1").getRootNode().appendChild(r[i]);
 			}
 			//var xnode=App.get(me.up('window'),"treepanel#T1").getStore().getNodeById(pp.data.id);
 			//console.log(xnode);
