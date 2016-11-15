d89a21c1ac1bdbc7bc99eff17d698a3d3eba3ce6
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-15 11:09:55 +0100

    Update# 457

--- src/System/app.js
+++ src/System/app.js
@@ -91,7 +91,7 @@ App = {
 				for (var i=objs.length-1;i>=0;i--) {
 					if (objs[i].parent==0) Obj=objs[i]; 
 					else {
-						console.log(objs[i-1]);
+						console.log(objs[i+1]);
 						if (!objs[i-1].children) objs[i-1].children=[];
 						objs[i-1].children.push(objs[i]);
 					};
