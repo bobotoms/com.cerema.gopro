a24e28a549ad2d435fdfc2b76d6b04513e42086f
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-15 09:40:38 +0100

    Update# 412

--- src/System/app.js
+++ src/System/app.js
@@ -40,45 +40,43 @@ App = {
 			var db=App.using('db');
 			var ff=[];	
 			var O={};
-			//if (req.body.node=="root") {
-				var sql="SELECT idElement,parent,new_elements.idType_element,new_elements.nomElement,modif FROM new_elements join types_elements on new_elements.idType_element=types_elements.idType where new_elements.idType=1 order by parent";
-				var db=Elements.using('db');
-				db.query("gopro",sql,function(e,r){
-					var root=[];
-					var obj={};
-					for (var i=0;i<r.length;i++) {
-						var id=r[i].idElement;
-						var parent=r[i].parent;
-						if (!obj[id]) obj[id]={
-							id: "c"+i,
-							name: "c"+i,
-							text: r[i].nomElement,
-							leaf: true
-						};
-						O[id]={
-							id: "c"+i,
-							name: "c"+i,
-							text: r[i].nomElement,
-							parent: parent
-						};
-						if (parent==0) root.push(obj[id]); else {
-							if (!obj[parent].children) {
-								obj[parent].children=[];
-								obj[parent].leaf=false;
-							};
-							obj[parent].children.push(obj[id]);
-						}
+			var sql="SELECT idElement,parent,new_elements.idType_element,new_elements.nomElement,modif FROM new_elements join types_elements on new_elements.idType_element=types_elements.idType where new_elements.idType=1 order by parent";
+			var db=Elements.using('db');
+			db.query("gopro",sql,function(e,r){
+				var root=[];
+				var obj={};
+				for (var i=0;i<r.length;i++) {
+					var id=r[i].idElement;
+					var parent=r[i].parent;
+					if (!obj[id]) obj[id]={
+						id: "c"+i,
+						name: "c"+i,
+						text: r[i].nomElement,
+						leaf: true
 					};
-					var itemId=1;
-					var i=1;
-					var objs=[];
-					while (itemId!=0) {
-						objs.push(O[itemId]);
-						itemId=O[itemId].parent;
+					O[id]={
+						id: "c"+i,
+						name: "c"+i,
+						text: r[i].nomElement,
+						parent: parent
 					};
-					res.end(JSON.stringify(objs,null,4));
-				});				
-			//}
+					if (parent==0) root.push(obj[id]); else {
+						if (!obj[parent].children) {
+							obj[parent].children=[];
+							obj[parent].leaf=false;
+						};
+						obj[parent].children.push(obj[id]);
+					}
+				};
+				var itemId=19;
+				var i=1;
+				var objs=[];
+				while (itemId!=0) {
+					objs.push(O[itemId]);
+					itemId=O[itemId].parent;
+				};
+				res.end(JSON.stringify(objs,null,4));
+			});
 		});		
 	}
 };
