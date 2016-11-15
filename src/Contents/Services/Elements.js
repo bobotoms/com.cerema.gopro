08704487a2aa2514fe06c79abe79e41bab6790fc
Stéphane Zucatti <stephane.zucatti@me.com>
2016-11-15 11:50:46 +0100

    Update# 478

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -1,34 +1,4 @@
 Elements={
-	getTree: function(o,cb) {
-		var sql="SELECT idElement,parent,new_elements.idType_element,new_elements.nomElement,modif FROM new_elements join types_elements on new_elements.idType_element=types_elements.idType where parent=0 and new_elements.idType=1";
-		var db=Elements.using('db');
-
-		db.query("gopro",sql,function(e,r){
-			console.log(e);
-			console.log(r);
-			var root=[];
-			var obj={};
-			for (var i=0;i<r.length;i++) {
-				var id=r[i].idElement;
-				var parent=r[i].parent;
-				if (!obj[id]) obj[id]={
-					id: i,
-					name: "c"+i,
-					text: r[i].nomElement,
-					leaf: true
-				};
-				if (parent==0) root.push(obj[id]); /*else {
-					if (!obj[parent].children) {
-						obj[parent].children=[];
-						obj[parent].leaf=false;
-					};
-					obj[parent].children.push(obj[id]);
-				}*/
-			}
-			cb(root);
-		});
-
-	},
 	getSelect: function(itemId,idType,cb) {
 		var db=Elements.using('db');
 		var ff=[];	
@@ -76,6 +46,7 @@ Elements={
 					delete objs[i].parent;
 				} else {
 					objs[i].leaf=true;
+					objs[i].id=
 					delete objs[i].parent;
 				};
 			};
