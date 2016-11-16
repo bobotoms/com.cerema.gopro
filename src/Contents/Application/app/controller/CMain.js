aff2c9c7bb9438ba8ac055609f2845a7e486240e
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-16 15:53:08 +0100

    Update# 678

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -81,7 +81,7 @@ App.controller.define('CMain', {
 	},
 	charact_cboFamille_select: function(me,store) {
 		App.get(me.up('window'),'combo#cboType').setValue('');
-		App.get(me.up('window'),'combo#cboType').getStore().getProxy().extraParams.type=store.data.idFamille;
+		App.get(me.up('window'),'combo#cboType').getStore().getProxy().extraParams.idFamille=store.data.idFamille;
 		App.get(me.up('window'),'combo#cboType').getStore().load();	
 	},	
 	charact_cbotype_select: function(me,store) {
