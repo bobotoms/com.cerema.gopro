f008f0f8dc02e762646b246a1216099b9be8bea1
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-16 10:50:34 +0100

    Update# 627

--- src/System/app.js
+++ src/System/app.js
@@ -3,37 +3,7 @@ App = {
 		app.use('/tmp',server.static(__dirname + require('path').sep+'tmp'));	
 		app.post('/evt',function(req,res){
 			res.header("Content-Type", "application/json; charset=utf-8");
-			var db=App.using('db');
-			var ff=[];	
 			
-			if (req.body.node=="root") {
-				var sql="SELECT idElement,parent,elements.idType_element,elements.nomElement,modif FROM elements join types_elements on elements.idType_element=types_elements.idType where elements.idType="+req.body.type+" order by parent";
-				
-				var db=Elements.using('db');
-				db.query("gopro",sql,function(e,r){
-					var root=[];
-					var obj={};
-					for (var i=0;i<r.length;i++) {
-						var id=r[i].idElement;
-						var parent=r[i].parent;
-						if (!obj[id]) obj[id]={
-							id: id,
-							name: "c"+id,
-							text: r[i].nomElement,
-							leaf: true
-						};
-						if (parent==0) root.push(obj[id]); else {
-							if (!obj[parent].children) {
-								obj[parent].children=[];
-								obj[parent].leaf=false;
-							};
-							obj[parent].children.push(obj[id]);
-						}
-					};
-					
-					res.end(JSON.stringify(root));
-				});				
-			}
 		});
 	
 	}
