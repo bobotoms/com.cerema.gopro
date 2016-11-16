f822780b9a54cc09a56059d26df261bc7532b876
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-16 11:22:13 +0100

    Update# 635

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -117,12 +117,12 @@ App.controller.define('CMain', {
 		
 	},
 	VAddItem_onShow: function(me) {
-		alert(me.type_item);
-		App.get(me,'treepanel#T0').getStore().getProxy().extraParams.type=me.type_item;
+		App.get(me,'treepanel#T0').getStore().getProxy().extraParams._type=me.type_item;
 		App.get(me,'treepanel#T0').getStore().load();
-		App.get(me,'treepanel#T0').getStore().on('load',function(){
+		console.log(App.get(me,'treepanel#T0').getStore().getProxy());
+		/*App.get(me,'treepanel#T0').getStore().on('load',function(){
 			App.get(me,'treepanel#T0').expandAll();
-		});
+		});*/
 		/*App.Elements.getTree({},function(response){
 
 			/*var json = response;
