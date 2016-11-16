fa1d44a30e679197a4d7780d73435ed0a143d944
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-16 10:50:29 +0100

    Update# 626

--- src/Contents/Services/Elements.js
+++ src/Contents/Services/Elements.js
@@ -1,4 +1,34 @@
 Elements={
+	getAllByType: function(type,cb) {
+		var db=Elements.using('db');
+		var ff=[];	
+			var sql="SELECT idElement,parent,elements.idType_element,elements.nomElement,modif FROM elements join types_elements on elements.idType_element=types_elements.idType where elements.idType="+type+" order by parent";
+
+			var db=Elements.using('db');
+			db.query("gopro",sql,function(e,r){
+				var root=[];
+				var obj={};
+				for (var i=0;i<r.length;i++) {
+					var id=r[i].idElement;
+					var parent=r[i].parent;
+					if (!obj[id]) obj[id]={
+						id: id,
+						name: "c"+id,
+						text: r[i].nomElement,
+						leaf: true
+					};
+					if (parent==0) root.push(obj[id]); else {
+						if (!obj[parent].children) {
+							obj[parent].children=[];
+							obj[parent].leaf=false;
+						};
+						obj[parent].children.push(obj[id]);
+					}
+				};
+
+				cb(root);
+			});				
+	},
 	getSelect: function(itemId,idType,cb) {
 		var db=Elements.using('db');
 		var ff=[];	
