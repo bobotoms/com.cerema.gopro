41ac631470be492c76a47b3a75d2164cea0fdec2
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-17 16:52:33 +0100

    Update# 844

--- src/Contents/Services/sql/OAGetAll.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM ouvrages 
-	left join familles on ouvrages.idFamille=familles.idFamille 
-    left join types on types.idType=ouvrages.idType
-    left join geologies on geologies.idGeologie=ouvrages.idGeologie
-    left join situations on situations.idSituation=ouvrages.idSituation
-    left join acces on acces.idAcces=ouvrages.idAcces
