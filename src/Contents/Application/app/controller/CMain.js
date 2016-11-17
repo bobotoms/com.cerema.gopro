6a54098275da4818ebb59d0299944c15af5cedc3
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-17 15:03:43 +0100

    Update# 816

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -136,7 +136,7 @@ App.controller.define('CMain', {
 				obj.source=source;
 				obj.sourceConfig=sourceConfig;
 				var grid2=Ext.create('Ext.grid.property.Grid',obj);
-				gridPanel.removeAll();
+				grid.remove();
 				gridPanel.add(grid2);
 			});
 		});
