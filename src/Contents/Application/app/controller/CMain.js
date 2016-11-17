412174dd2c21d15009ed268d5c06b454e817f212
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-17 10:04:41 +0100

    Update# 761

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -103,9 +103,9 @@ App.controller.define('CMain', {
 				e[r.data[i].COLUMN_NAME]='';
 			};
 			console.log(e);
-			e.idType=App.get(me.up('panel').up('panel'),"combo#cboType").getValue();
+			e.idType=App.get(me.up('panel').up('panel'),"tree#T0").getValue();
 			console.log(e);
-			App.get('VCharacteristics grid#T1').getStore().insert(0, e);
+			App.get(me.up('panel'),'grid#T1').getStore().insert(0, e);
 		});	
 	},
 	charact_grid_edit: function(ed,o) {
