3d7ef1edd2cebd7e3dc067701d2fd7209a531d2e
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-17 15:41:00 +0100

    Update# 827

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -100,8 +100,7 @@ App.controller.define('CMain', {
 		
 	},
 	new_ouvrage_record: function(me) {
-		alert(App.get(me.up('window')));
-		App.DB.post('gopro://ouvrages',App.get(me.up('window')),function(r){
+		App.DB.post('gopro://ouvrages',me.up('window'),function(r){
 			console.log(r);
 		});	
 	},
