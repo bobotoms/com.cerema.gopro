43eb396f14a61ccd5484a8982c2ddef441b47edc
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-18 10:53:29 +0100

    Update# 910

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -103,6 +103,22 @@ App.controller.define('CMain', {
 	},
 	VSaisie_onShow: function(me) {
 		if (me.idOuvrage) {
+			function getElements(PARAM,ndx,cb) {
+				App.Elements.getSelect(PARAM[ndx],App.get(me,"combo#type").getValue(),function(r){
+					if (!r[r.length-1].leaf) r[r.length-1].text="<b>"+r[r.length-1].text+"</b>";
+					console.log(r);
+					for (var i=0;i<r.length;i++) {	
+						var xnode=App.get(me,"treepanel").getRootNode().store.getNodeById('c'+r[i].parent);
+						if (!xnode) {
+							if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) App.get(me,"treepanel").getRootNode().appendChild(r[i]); 
+						} else {
+							if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) xnode.appendChild(r[i]);
+						};
+						App.get(me,"treepanel").expandAll();
+						if (ndx+1<PARAM.length) getElements(PARAM,ndx+1,cb); else cb();
+					}
+				});					
+			};
 			var store=App.store.create('gopro://types',{autoLoad:true});
 			App.get(me,'combo#type').bindStore(store);
 			App.get(me,'combo#type').getStore().load();
@@ -116,33 +132,10 @@ App.controller.define('CMain', {
 					if (r.data.length>0) {
 						for (var i=0;i<r.data.length;i++) {
 							PARAM.push(r.data[i].idElement);
-						}
-						App.Elements.getSelect(PARAM[0],App.get(me,"combo#type").getValue(),function(r){
-							if (!r[r.length-1].leaf) r[r.length-1].text="<b>"+r[r.length-1].text+"</b>";
-							console.log(r);
-							for (var i=0;i<r.length;i++) {	
-								var xnode=App.get(me,"treepanel").getRootNode().store.getNodeById('c'+r[i].parent);
-								if (!xnode) {
-									if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) App.get(me,"treepanel").getRootNode().appendChild(r[i]); 
-								} else {
-									if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) xnode.appendChild(r[i]);
-								};
-								App.get(me,"treepanel").expandAll();
-								App.Elements.getSelect(PARAM[1],App.get(me,"combo#type").getValue(),function(r){
-							if (!r[r.length-1].leaf) r[r.length-1].text="<b>"+r[r.length-1].text+"</b>";
-							console.log(r);
-							for (var i=0;i<r.length;i++) {	
-								var xnode=App.get(me,"treepanel").getRootNode().store.getNodeById('c'+r[i].parent);
-								if (!xnode) {
-									if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) App.get(me,"treepanel").getRootNode().appendChild(r[i]); 
-								} else {
-									if (!App.get(me,"treepanel").getRootNode().store.getNodeById(r[i].id)) xnode.appendChild(r[i]);
-								};
-								App.get(me,"treepanel").expandAll();
-							}
+						};
+						getElements(PARAM,0,function(){
+							console.log('all done.')
 						});
-							}
-						});						
 					}
 				});
 			});
