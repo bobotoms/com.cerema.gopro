5905bf3f69ed4824d115fea9081c1ff174fe909a
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-22 11:43:41 +0100

    Update# 960

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -143,11 +143,11 @@ App.controller.define('CMain', {
 			App.get(me,'combo#famille').setDisabled(true);
 			App.get(me,'combo#type').setDisabled(true);
 			// On charge les premiers items
-			App.DB.get('gopro://ouvrages?idOuvrage='+me.idOuvrage,me,function(r){
-				console.log(r);
-				return;
+			//App.DB.get('gopro://ouvrages?idOuvrage='+me.idOuvrage,me,function(r){
 				// On continue par les éléments
 				App.DB.get('gopro://oa_elements{idElement,nomOAElement}?idOuvrage='+me.idOuvrage,function(r){
+									console.log(r);
+				return;
 					var PARAM=[];
 					var PARAMX=[];
 					if (r.data.length>0) {
@@ -160,7 +160,7 @@ App.controller.define('CMain', {
 						});
 					}
 				});
-			});
+			//});
 		};
 	},
 	gridO_select: function(me,store) {
