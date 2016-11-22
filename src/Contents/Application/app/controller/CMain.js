0562ca4afb145762833cea5042f2761f1e9d05c6
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-11-22 11:41:32 +0100

    Update# 957

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -149,6 +149,7 @@ App.controller.define('CMain', {
 				App.DB.get('gopro://oa_elements?idOuvrage='+me.idOuvrage,function(r){
 					var PARAM=[];
 					var PARAMX=[];
+					console.log(r);
 					if (r.data.length>0) {
 						for (var i=0;i<r.data.length;i++) {
 							PARAM.push(r.data[i].idElement);
